1649236967df851c2efb327fa4990fdbb79bbf0e
kahil nayton <kahilnayton@gmail.com>
2019-04-04 21:23:49 -0400

    push

--- js/script.js
+++ js/script.js
@@ -1,199 +1,208 @@
 
 window.onload = function () {
     newGame();
+    scoreZero();
+
 }
 
 // const canvas = document.getElementById("canvas")
 // ctx is the context 
 // const txt = canvas.getContext('2d')
 
-let sizeInput = document.getElementById("size");
 let changeScore = document.getElementById("score")
 
 let score = 0
 let width = 100
 
 let fontSize;
-let loss = false;
+// let loss = false;
 
 let grid = []
 let size = 4;
-
-
-// document.onKeyDown(event => {
-//     if (event.keyCode === 39) {
-
-
+let flipped = false
 
 
 let playbutton = document.getElementById('playbutton')
+playbutton.addEventListener('click', () => newGame())
 
-
-playbutton.addEventListener('click', () => get2RandomCells())
-
-
+let welcome = document.querySelector('.welcome')
+welcome.addEventListener('click', () => {
+    welcome.style.display = "none";
+})
 
 
 function newGame() {
-    createCells()
-    drawAllCells()
-    createCell()
-    createCell()
-    // drawCell()
-}
-
-
-
+        scoreZero()
+        createCells()
+        drawAllCells()
+        createCell()
+        createCell()
+    }
 
 function cell(row, column) {
-    this.value = 0
-    this.x = column
-    this.y = row
-}
-
-
+        this.value = 0
+        this.x = column
+        this.y = row
+    }
 
 function createCells() {
-    for (let i = 0; i < size; i++) {
-        grid[i] = [];
-        for (let j = 0; j < size; j++) {
-            let newCell = new cell(i, j)
-            grid[i][j] = newCell
-            drawCell(newCell)
-        }
-    }
-
-}
-
-
-function drawAllCells() {
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            drawCell(grid[i][j]);
-        }
-    }
-}
-
-// creates random cell
-function createCell() {
-    // tried to make a game over function
-    let freeCell = 0;
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            if (!grid[i][j].value) {
-                freeCell++;
+        for (let i = 0; i < size; i++) {
+            grid[i] = [];
+            for (let j = 0; j < size; j++) {
+                let newCell = new cell(i, j)
+                grid[i][j] = newCell
+                drawCell(newCell)
             }
         }
     }
-    if (!freeCell) {
-        gameOver();
-        return;
+
+
+function drawAllCells() {
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                drawCell(grid[i][j]);
+            }
+        }
     }
-    while (true) {
-        let options = []
-        let i = Math.floor(Math.random() * 4)
-        let j = Math.floor(Math.random() * 4)
-        const getCell = grid[i][j]
 
-        let r = Math.random();
 
-        let value = r < 0.5 ? 2 : 4;
-        getCell.value = value
+// creates random cell
+function createCell() {
+        // tried to make a game over function
+        let freeCell = 0;
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                if (!grid[i][j].value) {
+                    freeCell++
+                }
+            }
+        }
 
-        drawCell(getCell)
-        return
+        if (!freeCell) {
+            gameOver();
+            return;
 
+        }
+        while (true) {
+            let i = Math.floor(Math.random() * 4)
+            let j = Math.floor(Math.random() * 4)
+            const getCell = grid[i][j]
+
+            if (getCell.value === 0) {
+                let r = Math.random();
+                let value = r < 0.5 ? 2 : 4;
+                getCell.value = value
+                drawCell(getCell)
+                return
+            }
+        }
     }
-}
-// function keyRight() {
-//     document.addEventListener("keydown", event => {
-//         if (event.isComposing || event.keyCode === 39) {
-
-//         })
-// }
-
 
 
 // create a slide function , make array
 function slide(row) {
-    let arr = row.filter(row => row > 1)
-    let empty = (4 - arr.length);
-    let zeros = Array(empty).fill(0);
-    arr = zeros.concat(arr)
-    console.log(arr)
+        let arr = row.filter(row => row > 1)
+        let empty = (4 - arr.length);
+        let zeros = Array(empty).fill(0);
+        arr = zeros.concat(arr)
+        console.log(arr)
 
-    return arr;
-}
-
-// function updateDomBoard() {
-// for (let i = 0; i < size; i++) {
-//     cells[i] = [];
-//     for (let j = 0; j < size; j++) {
-
-
-// document.getElementById("p1").innerHTML = "New text!";
-
-
-
-
-
+        return arr;
+    }
 
 
 // This should make the number on the left become zero and the new number twice what it was, operating on array itself
 function combine(row) {
-    for (let i = 3; i >= 0; i--) {
-        let a = row[i];
-        let b = row[i - 1]
-        if (a == b) {
-            row[i] = a + b
-            row[i - 1] = 0
+        for (let i = 3; i >= 0; i--) {
+            let a = row[i];
+            let b = row[i - 1]
+            if (a == b) {
+                row[i] = a + b
+                row[i - 1] = 0
 
-            break
+                break
+            }
+
         }
+        return row;
 
     }
-    return row;
-
-}
 
 
 
-//  Combines and slides  
+
+//  Combines and slides row 
 function slideCombine(row) {
-    row = slide(row)
-    row = combine(row)
-    row = slide(row)
-    row = combine(row)
+        row = slide(row)
+        row = combine(row)
+        row = slide(row)
+        // row = combine(row)
 
-    return row;
+        return row;
 
-}
-
-
+    }
 
 
-//  move right and updates dom
+function flip() {
+        for (let i = 0; i < size; i++) {
+            grid[i].reverse()
+
+        }
+    }
+
+function flipTwo(array) {
+        for (let i = 0; i < size; i++) {
+            array[i].reverse()
+
+        }
+    }
+
+function transposeArray(array) {
+        let newArray = [];
+        for (let i = 0; i < array.length; i++) {
+            newArray.push([]);
+        };
+
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[0].length; j++) {
+                newArray[j].push(array[i][j]);
+            };
+        };
+
+        return newArray;
+    }
+
 function moveRight() {
-    let pastGrid = copy(grid)
-    for (let i = 0; i < size; i++) {
-        let currentRow = grid[i]
-        let newArrayOfNumbers = currentRow.map((obj) => {
-            return obj.value
+        moveMatrixTwo(transposeArray(grid));
+    }
 
-        })
+function moveLeft() {
+        moveMatrixTwo(transposeArray(grid));
+        flipTwo(transposeArray(grid))
+        flipTwo(transposeArray(grid))
+        moveMatrixTwo(transposeArray(grid));
 
-        let slidArray = slide(newArrayOfNumbers)
+    }
 
-        for (let j = 0; j < size; j++) {
-            currentRow[j].value = slidArray[j]
-            drawCell(currentRow[j])
+
+
+function moveMatrixTwo(gridArray) {
+        for (let i = 0; i < size; i++) {
+            let currentRow = gridArray[i]
+            // takes the value of each obj and assigns it to a number
+            let newArrayOfNumbers = currentRow.map((obj) => {
+                return obj.value
+            })
+
+            let slidArray = slideCombine(newArrayOfNumbers)
+            for (let i = 0; i < size; i++) {
+                currentRow[i].value = slidArray[i]
+                drawCell(currentRow[i])
+            }
         }
         createCell()
-        // taking each value in slid array -
-        // updating the current row
-
     }
-}
+
 
 
 // get all 4 rows in your grid 
@@ -201,150 +210,164 @@ function moveRight() {
 // pass into slide combine 
 // with new numbers upadate by updating row 
 // update all of the cells 
+//  move right and updates dom
+function moveMatrix() {
+        // let pastGrid = copy(grid)
+        for (let i = 0; i < size; i++) {
+            let currentRow = grid[i]
+            // takes the value of each obj and assigns it to a number
+            let newArrayOfNumbers = currentRow.map((obj) => {
+                return obj.value
+            })
+
+            let slidArray = slideCombine(newArrayOfNumbers)
+            for (let i = 0; i < size; i++) {
+                currentRow[i].value = slidArray[i]
+                drawCell(currentRow[i])
+            }
+        }
+        createCell()
+    }
+
+function moveUp() {
+        flip()
+        moveMatrix();
+        flip()
+    }
 
 
-// slideCombine()
-
-// let i, row
-
-// cycle through i & j to see which tiles are o
-// for (let i = 0; i < size; i++) {
-//     row = operate(row)
-
-// }
-// for (let j = 0; j < size; i++) {
-//   
-//     createCell()
-
-// }
-
-// function operate(row) {
-//     grid[i] = slide(grid[i])
-//     grid[i] = combine(grid[i])
-//     grid[i] = slide(gride[i])
-//     return row;
-// }
-
-
-
+//  move up
 
 
 //  move left and updates dom
+// create new combine method for j index 
+// create combine on j axis 
+
 // function moveLeft() {
-//     for (let i = 0; i < 4; i++) {
-//         grid[i] = slide(grid[i])
-//         console.log(grid[i])
+//     for (let j = 0; j < size; j++) {
+//         let currentColl = grid[j]
+//         // takes the value of each obj and assigns it to a number
+//         let newArrayOfNumbers = currentColl.map((obj) => {
+//             return obj.value
+
+//         })
+
+//         let slidArray = slideCombineColl(newArrayOfNumbers)
+
+//         for (let j = 0; j < size; j++) {
+//             currentColl[j].value = slidArray[j]
+//             drawCell(currentColl[j])
+//         }
+
 //     }
-
+//     createCell()
 // }
-
-//  move up
-// function moveUp() {
-//     for (let i = 0; i < 4; i++) {
-//         grid[i] = slide(grid[i])
-//         console.log(grid[i])
-//     }
-
-// }
-
-
-
-//  move down
-// function moveDown() {
-//     for (let i = 0; i < 4; i++) {
-//         grid[i] = slide(grid[i])
-//         console.log(grid[i])
-//     }
-
-// }
-
 
 
 // takes in keys 
 document.addEventListener('keydown', event => {
-    if (event.keyCode === 38) {
-        moveUp();
-    } else if (event.keyCode === 39) {
-        moveRight();
-    } else if (event.keyCode === 40) {
-        moveDown();
-    } else if (event.keyCode === 37) {
-        moveLeft();
-    }
-    // scoreLabel.innerHTML = 'Score : ' + score;
-})
+    event.preventDefault();
 
+        // move up prevent default 
+        if (event.keyCode === 38) {
+            moveUp()
+            // move down
+        } else if (event.keyCode === 40) {
+            moveMatrix();
+            // move right 
+        } else if (event.keyCode === 39) {
+            moveRight();
+            // move left
+        } else if (event.keyCode === 37) {
+            moveLeft();
+        }
 
+        // let oldScore = document.getElementById('score').innerHTML 
+        // let oldScore = document.getElementById('score').innerHTML 
 
+        // let newScore = oldScoreScore 
+
+    })
 
 
 function drawCell(cell) {
 
-    let id = cell.x * 4 + cell.y;
-    let newCell = document.getElementById(String(id))
+        let id = cell.x * 4 + cell.y;
+        let newCell = document.getElementById(String(id))
+        // let animation = document.querySelector(".cell")
 
-    // console.log(newCell, id)
+        // console.log(newCell, id)
 
-    switch (cell.value) {
+        switch (cell.value) {
 
-        case 0:
-            newCell.style.backgroundColor = "white";
-            break;
-        case 2:
-            newCell.style.backgroundColor = "#E6B0AA";
-            break;
-        case 4:
-            newCell.style.backgroundColor = "#CD6155";
-            break;
-        case 8:
-            newCell.style.backgroundColor = "#A93226";
-            break;
-        case 16:
-            newCell.style.backgroundColor = "#7B241C";
-            break;
-        case 32:
-            newCell.style.backgroundColor = "#F2D7D5";
-            break;
-        case 64:
-            newCell.style.backgroundColor = "#D98880";
-            break;
-        case 128:
-            newCell.style.backgroundColor = "#C0392B";
-            break;
-        case 256:
-            newCell.style.backgroundColor = "#D7BDE2";
-            break;
-        case 512:
-            newCell.style.backgroundColor = "#A9CCE3";
-            break;
-        case 1024:
-            newCell.style.backgroundColor = "#AED6F1";
-            break;
-        case 2048:
-            newCell.style.backgroundColor = "#A3E4D7";
-            break;
-        case 4096:
-            newCell.style.backgroundColor = "#A9DFBF";
-            break;
+            case 0:
+                newCell.style.backgroundColor = "white";
+                break;
+            case 2:
+                newCell.style.backgroundColor = "#E6B0AA";
+                // animation.style.scale-down-center
+                break;
+            case 4:
+                newCell.style.backgroundColor = "#CD6155";
+                break;
+            case 8:
+                newCell.style.backgroundColor = "#A93226";
+                break;
+            case 16:
+                newCell.style.backgroundColor = "#7B241C";
+                break;
+            case 32:
+                newCell.style.backgroundColor = "#F2D7D5";
+                break;
+            case 64:
+                newCell.style.backgroundColor = "#D98880";
+                break;
+            case 128:
+                newCell.style.backgroundColor = "#C0392B";
+                break;
+            case 256:
+                newCell.style.backgroundColor = "#D7BDE2";
+                break;
+            case 512:
+                newCell.style.backgroundColor = "#A9CCE3";
+                break;
+            case 1024:
+                newCell.style.backgroundColor = "#AED6F1";
+                break;
+            case 2048:
+                newCell.style.backgroundColor = "#A3E4D7";
+                break;
+            case 4096:
+                newCell.style.backgroundColor = "#A9DFBF";
+                break;
+        }
+        if (cell.value === 0) {
+            newCell.innerText = ''
+        } else {
+            newCell.innerText = cell.value
+            score += cell.value
+            countScore(score)
+        }
+
     }
-    if (cell.value === 0) {
-        newCell.innerText = ''
-    } else {
-        newCell.innerText = cell.value
-    }
-
-}
 
 // document.body.addEventListener("keydown", evt => {
 //     moveRight(evt)
 // })
+function countScore(score) {
+        document.querySelector("#score").innerHTML = score
 
+    }
 
 
 function gameOver() {
-    document.getElementById(table).style.opacity = 0;
-    looser = true;
-}
+        document.querySelector("#go-label").style.opacity = "1"
+    }
+
+function scoreZero() {
+        document.querySelector("#score").innerHTML = 0
+    }
+
 
 // generates random location 
 
